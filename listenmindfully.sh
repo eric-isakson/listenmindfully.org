5da5aed0dc8f394c7a94da6827671cfef2f2155f
Eric Isakson <eisakson@nc.rr.com>
2014-11-13 16:41:20 -0500

    clone site from github and change ownership of /var/www to root

--- listenmindfully.sh
+++ listenmindfully.sh
@@ -639,9 +639,6 @@ function node_init_script {
     groupadd -r $NAME
     useradd -r -g $NAME -G syslog $NAME
 
-    chown -R $NAME:$NAME /var/$NAME
-    chmod 755 /var/$NAME
-
 cat <<EOT >/etc/init.d/$NAME
 #! /bin/sh
 ### BEGIN INIT INFO
@@ -817,107 +814,8 @@ EOT
 }
 
 function www_install {
-
-    # TODO use git to clone the application source and place in /var/www
     git clone https://github.com/eric-isakson/listenmindfully.org.git /var/www
-    mkdir /var/www
-cat <<EOT >/var/www/app.js
-var http = require('http');
-var PORT = process.env.PORT || 3000;
-
-http.createServer(function (req, res) {
-  console.log('%d request received', process.pid);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world!\n');
-}).listen(PORT);
-
-console.log('%d listening on %d', process.pid, PORT);
-EOT
-
-cat <<EOT >/var/www/boot.js
-// This script will boot app.js with the number of workers
-// specified in WORKER_COUNT.
-//
-// The master will respond to SIGHUP, which will trigger
-// restarting all the workers and reloading the app.
-
-var cluster = require('cluster');
-var workerCount = process.env.WORKER_COUNT || 2;
-
-// Defines what each worker needs to run
-// In this case, it is app.js a simple node http app
-cluster.setupMaster({ exec: 'app.js' });
-
-// Gets the count of active workers
-function numWorkers() { return Object.keys(cluster.workers).length; }
-
-var stopping = false;
-
-// Forks off the workers unless the server is stopping
-function forkNewWorkers() {
-  if (!stopping) {
-    for (var i = numWorkers(); i < workerCount; i++) { cluster.fork(); }
-  }
-}
-
-// A list of workers queued for a restart
-var workersToStop = [];
-
-// Stops a single worker
-// Gives 60 seconds after disconnect before SIGTERM
-function stopWorker(worker) {
-  console.log('stopping', worker.process.pid);
-  worker.disconnect();
-  var killTimer = setTimeout(function() {
-    worker.kill();
-  }, 5000);
-
-  // Ensure we do not stay up just for this setTimeout
-  killTimer.unref();
-}
-
-// Tell the next worker queued to restart to disconnect
-// This will allow the process to finish work
-// for 60 seconds before sending SIGTERM
-function stopNextWorker() {
-  var i = workersToStop.pop();
-  var worker = cluster.workers[i];
-  if (worker) stopWorker(worker);
-}
-
-// Stops all the works at once
-function stopAllWorkers() {
-  stopping = true;
-  console.log('stopping all workers');
-  for (var id in cluster.workers) {
-    stopWorker(cluster.workers[id]);
-  }
-}
-
-// Worker is now listening on a port
-// Once it is ready, we can signal the next worker to restart
-cluster.on('listening', stopNextWorker);
-
-// A worker has disconnected either because the process was killed
-// or we are processing the workersToStop array restarting each process
-// In either case, we will fork any workers needed
-cluster.on('disconnect', forkNewWorkers);
-
-// HUP signal sent to the master process to start restarting all the workers sequentially
-process.on('SIGHUP', function() {
-  console.log('restarting all workers');
-  workersToStop = Object.keys(cluster.workers);
-  stopNextWorker();
-});
-
-// Kill all the workers at once
-process.on('SIGTERM', stopAllWorkers);
-
-// Fork off the initial workers
-forkNewWorkers();
-console.log('app master', process.pid, 'booted');
-
-EOT
+    chmod 755 /var/www
 
 cat <<EOT >/etc/default/www
 NODE_ENV="production"
