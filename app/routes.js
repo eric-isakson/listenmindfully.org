cd87b55197ce5545c2f695fda83f681f037c08e9
Eric Isakson <eisakson@nc.rr.com>
2014-11-21 00:22:16 -0500

    Move static content and error handling after route definitions.

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,8 @@
 /**
  * Module dependencies.
  */
-var IoC = require('electrolyte');
+var IoC = require('electrolyte')
+    , express = require('express');
 
 
 /**
@@ -13,6 +14,35 @@ var IoC = require('electrolyte');
  */
 module.exports = function routes() {
 
-    this.all('*', IoC.create('handlers/all'));
+    this.all('/', IoC.create('handlers/all'));
+
+    // static content ==============================================================
+    this.use(express.static('public'));
+
+    // errors ======================================================================
+    // catch 404 and forward to error handler
+    this.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handlers
+
+    // development error handler
+    // will print stacktrace
+    if (this.get('env') === 'development') {
+        this.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.type('text');
+            res.send(err.status + ' ' + err.message + '\n' + err.stack);
+        });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
+    this.use(function (err, req, res, next) {
+        res.sendStatus(err.status || 500);
+    });
 
 };
--- etc/init/02_middleware.js
+++ etc/init/02_middleware.js
@@ -16,31 +16,5 @@ module.exports = function () {
     this.use(bodyParser.json());
     this.use(bodyParser.urlencoded({ extended: true }));
     this.use(methodOverride());
-    this.use(express.static('public'));
 
-    // errors ======================================================================
-    // catch 404 and forward to error handler
-    this.use(function(req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    });
-
-    // error handlers
-
-    // development error handler
-    // will print stacktrace
-    if (this.get('env') === 'development') {
-        this.use(function (err, req, res, next) {
-            res.status(err.status || 500);
-            res.type('text');
-            res.send(err.status + ' ' + err.message + '\n' + err.stack);
-        });
-    }
-
-    // production error handler
-    // no stacktraces leaked to user
-    this.use(function (err, req, res, next) {
-        res.sendStatus(err.status || 500);
-    });
 };
