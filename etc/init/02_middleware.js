dd3200d496147c114e6a1d51f6588bb94abd1467
Eric Isakson <eisakson@nc.rr.com>
2014-11-20 22:59:45 -0500

    Add middleware libraries

--- etc/init/02_middleware.js
+++ etc/init/02_middleware.js
@@ -2,15 +2,19 @@
  * Module dependencies.
  */
 var express = require('express')
-    , path = require('path');
+    , path = require('path')
+    , cookieParser = require('cookie-parser')
+    , bodyParser = require('body-parser')
+    , session = require('express-session');
 
 /**
  * Initialize middleware.
  */
 module.exports = function () {
 
-    this.use(express.urlencoded());
-    this.use(express.json());
+    this.use(cookieParser());
+    this.use(bodyParser.json());
+    this.use(bodyParser.urlencoded({ extended: true }));
     this.use(this.router);
     this.use(express.static(path.join(__dirname, 'public')));
     this.use(express.errorHandler());
