c9ca8968b01c50284603b7353ff60ca8a83d3066
Eric Isakson <eisakson@nc.rr.com>
2014-11-20 23:20:16 -0500

    more middleware fixes

--- etc/init/02_middleware.js
+++ etc/init/02_middleware.js
@@ -5,6 +5,8 @@ var express = require('express')
     , path = require('path')
     , cookieParser = require('cookie-parser')
     , bodyParser = require('body-parser')
+    , errorHandler = require('errorHandler')
+    , methodOverride = require('method-override')
     , session = require('express-session');
 
 /**
@@ -15,8 +17,8 @@ module.exports = function () {
     this.use(cookieParser());
     this.use(bodyParser.json());
     this.use(bodyParser.urlencoded({ extended: true }));
-    this.use(this.router);
+    this.use(methodOverride());
     this.use(express.static(path.join(__dirname, 'public')));
-    this.use(express.errorHandler());
+    this.use(errorHandler());
 
 };
