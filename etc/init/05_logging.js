31d294f8f055fcc6ad061295cb37dea019032b70
Eric Isakson <eisakson@nc.rr.com>
2014-11-25 22:06:36 -0500

    Fix express-winston middleware configuration.

--- etc/init/05_logging.js
+++ etc/init/05_logging.js
@@ -11,21 +11,21 @@ module.exports = function () {
     var logger = IoC.create('logger');
 
     this.use(expressWinston.logger({
-        transports: logger.transports,
+        winstonInstance: logger,
         meta: true,
         msg: '{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}',
         colorStatus: true
     }));
 
-    // limit the logging information in the auth routes
-    this.all('/auth', function (req, res, next) {
-        req._routeWhitelists.req = [];
-        req._routeWhitelists.body = [];
-        req._routeWhitelists.res = ['_headers'];
-        next();
-    });
+    // TODO any limits we need on the logging information in the auth routes
+//    this.all('/auth', function (req, res, next) {
+//        req._routeWhitelists.req = [];
+//        req._routeWhitelists.body = [];
+//        req._routeWhitelists.res = [];
+//        next();
+//    });
 
     this.use(expressWinston.errorLogger({
-        transports: logger.transports
+        winstonInstance: logger,
     }));
 };
