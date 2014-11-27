73a2639b2bd1ff078e74c3d609c2c10f127e7810
Eric Isakson <eisakson@nc.rr.com>
2014-11-26 22:33:45 -0500

    Updated logo.

--- public/ui/Main.controller.js
+++ public/ui/Main.controller.js
@@ -2,7 +2,7 @@ jQuery.sap.require('sap.m.MessageToast');
 
 sap.ui.controller('listenmindfully.ui.Main', {
     onInit: function () {
-        var oData = {logo: jQuery.sap.getModulePath('listenmindfully.ui', '/') + 'mimes/logo/logo_64x26.png'};
+        var oData = {logo: jQuery.sap.getModulePath('listenmindfully.ui', '/') + 'mimes/logo/logo_50x26.png'};
         var oModel = new sap.ui.model.json.JSONModel();
         oModel.setData(oData);
         this.getView().setModel(oModel);
